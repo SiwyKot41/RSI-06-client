82bd0bc0a500aa61bc88ae321bc3fda80c6a7e0c
SiwyKot41 <frutkowski80@gmail.com>
2023-04-13 17:56:21 +0200

    feat(client): add client

---
+++ src/main/java/org/fr/rsi/Main.java
@@ -0,0 +1,25 @@
+package org.fr.rsi;
+
+import org.fr.rsi.ImageServerImpl;
+import org.fr.rsi.ImageServerImplService;
+
+import javax.swing.*;
+import javax.xml.ws.soap.MTOMFeature;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ImageServerImplService service  = new ImageServerImplService();
+
+        ImageServerImpl port = service.getImageServerImplPort(new MTOMFeature());
+        byte[] image = port.downloadImage("mona-lisa.jpg");
+
+        JFrame frame = new JFrame();
+        frame.setSize(300, 300);
+        JLabel label = new JLabel(new ImageIcon(image));
+        frame.add(label);
+        frame.setVisible(true);
+
+        System.out.println("imageServer.downloadImage() : Download Successful");
+    }
+}
---
+++ src/main/java/org/fr/rsi/webservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://rsi.fr.org/")
+package org.fr.rsi;
